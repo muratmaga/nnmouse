306a3a9c51614063d611f1758525c40dfa6eff0d
Murat Maga <muratmaga@outlook.com>
2024-02-21 09:41:52 -0800

    editing the labels for skulls

--- mouse-prep.py
+++ mouse-prep.py
@@ -2,12 +2,12 @@ import glob
 import numpy
 import os
 
-basePath = "/mnt/mouse/data"
+basePath = "/home/maga/Desktop/nnmouse/"
 
-sourcePath = f"{basePath}/mouse_fetus"
+sourcePath = f"{basePath}/bl6_192"
 sourceLabelPath = f"{sourcePath}/labels/final"
 
-nnPath = f"{basePath}/nnmouse/raw/Dataset001_mouse/"
+nnPath = f"{basePath}/raw/Dataset001_mouse/"
 trainPath = f"{nnPath}/imagesTr"
 testPath = f"{nnPath}/imagesTs"
 labelPath = f"{nnPath}/labelsTr"
@@ -29,3 +29,18 @@ for ctFile in glob.glob(f"{sourcePath}/*.nii.gz"):
         os.system(f"ln -s {ctFile} {testPath}/{fileBase}_0000.nii.gz")
 print(f"{cts} cts and {labels} labels")
 print('done')
+
+# from mouse-remap.py
+# re-arrangine labelmaps
+
+for labelFile in glob.glob(f"{labelPath}/*.nii.gz"):
+    labelNode = slicer.util.loadVolume(labelFile)
+    labelArray = slicer.util.arrayFromVolume(labelNode)
+    for x in numpy.arange(5, 18, 1):
+      labelArray[labelArray == x] = x-4
+    #labelArray[labelArray == 102] = 49
+    #labelArray[labelArray == 103] = 50
+      slicer.util.arrayFromVolumeModified(labelNode)
+      slicer.util.saveNode(labelNode, labelFile)
+      slicer.mrmlScene.RemoveNode(labelNode)
+
--- mouse-remap.py
+++ mouse-remap.py
@@ -6,15 +6,17 @@
 import glob
 import numpy
 
-nnPath = f"{basePath}/nnmouse/raw/Dataset001_mouse/"
-labelPath = f"{nnPath}/labelsTr"
+#nnPath = f"{basePath}/nnmouse/raw/Dataset001_mouse/"
+#labelPath = f"{nnPath}/labelsTr"
 
 for labelFile in glob.glob(f"{labelPath}/*.nii.gz"):
     labelNode = slicer.util.loadVolume(labelFile)
     labelArray = slicer.util.arrayFromVolume(labelNode)
-    labelArray[labelArray == 102] = 49
-    labelArray[labelArray == 103] = 50
-    slicer.util.arrayFromVolumeModified(labelNode)
-    slicer.util.saveNode(labelNode, labelFile)
-    slicer.mrmlScene.RemoveNode(labelNode)
+    for x in numpy.arange(5, 18, 1):
+      labelArray[labelArray == x] = x-4
+    #labelArray[labelArray == 102] = 49
+    #labelArray[labelArray == 103] = 50
+      slicer.util.arrayFromVolumeModified(labelNode)
+      slicer.util.saveNode(labelNode, labelFile)
+      slicer.mrmlScene.RemoveNode(labelNode)
 
